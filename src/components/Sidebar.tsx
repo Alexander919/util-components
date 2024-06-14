236d565c7dd434aeec02d585bc6742d6fc3625ee
Aleksandr Egorov <a.jegorov84@gmail.com>
2024-06-14 13:43:06 +0100

    add Sidebar component

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import Link from "./AnchorComponent/Link.tsx";
+
+type SidebarLinkType = {
+    label: string;
+    path: string;
+}
+
+function Sidebar() {
+    const links: SidebarLinkType[] = [
+        { label: "Home", path: "/" },
+        { label: "Accordion", path: "/accordion" },
+        { label: "Select", path: "/select" },
+        { label: "Table", path: "/table" },
+        { label: "TableGrider", path: "/table-grider" },
+    ];
+
+    const renderedLinks = links.map(lnk => {
+        return <Link key={lnk.label} to={lnk.path} activeClassName={"font-bold border-l-4 border-blue-400 px-1"}>{lnk.label}</Link>
+    });
+
+    return (
+        <div className={"sticky space-y-3 top-0 overflow-y-scroll flex flex-col items-start"}>
+            {renderedLinks}
+        </div>
+    );
+}
+
+export default Sidebar;
